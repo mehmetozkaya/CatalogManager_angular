56917602aa66fd4c341c8497fe40ea121083fdb9
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-21 23:43:28 +0300

    ratingClicked event emitted

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -67,4 +67,8 @@ export class ProductListComponent implements OnInit {
         return this.products.filter((product : IProduct) => 
                 product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
+
+    onRatingClicked(message : string) : void {
+        this.productCaption = 'Product List : ' + message;
+    }
 }
