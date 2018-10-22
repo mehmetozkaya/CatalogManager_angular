3c47830fb73f44ee98893937797b355695cb761f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-22 20:40:52 +0300

    refactor

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports: [    
+    RouterModule.forChild([
+      { path : 'products', component : ProductListComponent },
+      { path : 'products/:id', canActivate : [ProductDetailGuard], component : ProductDetailComponent }
+    ]),
+    SharedModule
   ],
-  declarations: []
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe    
+  ]
 })
 export class ProductModule { }
