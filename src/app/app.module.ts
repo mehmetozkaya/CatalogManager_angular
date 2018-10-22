077d87cd4ee1134eed7e97bfeb47b62799b8aaa7
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-22 18:19:31 +0300

    ProductDetailGuard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 
 @NgModule({
@@ -28,7 +29,7 @@ import { WelcomeComponent } from './home/welcome.component';
     HttpClientModule,
     RouterModule.forRoot([
       { path : 'products', component : ProductListComponent },
-      { path : 'products/:id', component : ProductDetailComponent },
+      { path : 'products/:id', canActivate : [ProductDetailGuard], component : ProductDetailComponent },
       { path : 'welcome', component : WelcomeComponent },
       { path : '', redirectTo : 'welcome', pathMatch : 'full' },
       { path : '**', redirectTo : 'welcome', pathMatch : 'full' }
